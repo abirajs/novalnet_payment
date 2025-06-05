c637567727c938c9b4f31f81f0ef2bdd778af58c
abirajs <meisterheidi89@gmail.com>
2025-06-05 10:22:06 +0530

    Update mock-payment.dto.ts

--- processor/src/dtos/mock-payment.dto.ts
+++ processor/src/dtos/mock-payment.dto.ts
@@ -24,21 +24,6 @@ export const PaymentRequestSchema = Type.Object({
     invoiceMemo: Type.Optional(Type.String()),
   }),
   paymentOutcome: PaymentOutcomeSchema,
-  merchant:  Type.Object({
-    signature: Type.Optional(Type.String()),
-    tariff: Type.Optional(Type.String()),
-  }),
-  customer: Type.Object({
-    first_name: Type.Optional(Type.String()),
-    last_name: Type.Optional(Type.String()),
-    email: Type.Optional(Type.String()),
- }),
-  transaction: Type.Object({
-    test_mode: Type.Optional(Type.String()),
-    payment_type: Type.Optional(Type.String()),
-    amount: Type.Optional(Type.String()),
-    currency: Type.Optional(Type.String()),
- }),
 });
 
 export type PaymentRequestSchemaDTO = Static<typeof PaymentRequestSchema>;
